b58f789c21cd7977d6d24e2397dce6ca11a0b432
Craig <craig.mwangi@student.moringaschool.com>
2024-03-22 18:09:57 +0300

    Labs

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,34 @@
 require ( './helpers.js' );
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+function fareDoubler(fare) {
+  return fare * 2;
+}
+function fareTripler(fare) {
+  return fare * 3;
+}
+function selectDifferentDrivers(drivers, selectionFunction) {
+  return selectionFunction(drivers);
+}
+
+
+
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+
+
 
 describe('index.js', function () {
   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
